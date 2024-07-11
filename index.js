93cda6895c4c7087806220681bc1c11a00458575
holgermueller <9jholger@gmail.com>
2024-07-11 15:03:33 -0500

    got dynamic buttons on dom

--- index.js
+++ index.js
@@ -3,3 +3,21 @@
 let occupied = [];
 let available = [];
 let selected = [];
+
+const createSeats = () => {
+  const seatsContainers = document.getElementsByClassName("seats-containers");
+
+  for (let i = 0; i < seatsContainers.length; i++) {
+    console.log(seatsContainers);
+
+    for (let j = 1; j <= 30; j++) {
+      const button = document.createElement("button");
+      button.classList.add("seat");
+      seatsContainers[i].appendChild(button);
+    }
+  }
+};
+
+const selectSeat = () => {};
+
+createSeats();
