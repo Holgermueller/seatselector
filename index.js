63a33b6720c08e922762e43560bb97b9ddf71bbc
holgermueller <9jholger@gmail.com>
2024-07-11 15:56:40 -0500

    got buttons id to console log

--- index.js
+++ index.js
@@ -8,16 +8,26 @@ const createSeats = () => {
   const seatsContainers = document.getElementsByClassName("seats-containers");
 
   for (let i = 0; i < seatsContainers.length; i++) {
-    console.log(seatsContainers);
-
     for (let j = 1; j <= 30; j++) {
       const button = document.createElement("button");
       button.classList.add("seat");
+      button.classList.add("available-seat");
+      button.id = i + "-" + j;
+
       seatsContainers[i].appendChild(button);
     }
   }
 };
 
-const selectSeat = () => {};
+const selectSeat = () => {
+  const seats = document.getElementsByClassName("seat");
+
+  for (let i = 0; i < seats.length; i++) {
+    seats[i].addEventListener("click", (event) => {
+      console.log(event.target.id);
+    });
+  }
+};
 
 createSeats();
+selectSeat();
